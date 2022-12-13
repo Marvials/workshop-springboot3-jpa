731f4a1b3ec539c4828ef9bc802b3613ed04434f
Marvials <marcosgaba7@gmail.com>
2022-12-12 21:49:50 -0300

    Entity Product

---
+++ src/main/java/com/marvi/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.marvi.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marvi.course.entities.Product;
+import com.marvi.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	 
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
